6cfd9b8a0bd2f6dd35d193016798aafa29354ae0
Nicholas Weber <nicholaseweber@gmail.com>
2016-09-25 15:02:37 -0700

    Add sass compile to gulp.

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,16 @@
 var gulp           = require( 'gulp' );
 var del            = require( 'del' );
 var nunjucksRender = require( 'gulp-nunjucks-render' );
+var sass           = require( 'gulp-sass' );
 
+//Delete old files in target
 gulp.task( 'clean', function () {
     return del( [
         'target/**/*'
     ] );
 } );
 
+//Compile nunjucks files, put in target
  gulp.task( 'nunjucks', function() {
      return gulp.src( 'pages/**/*.+(html|nunjucks)' )
      .pipe( nunjucksRender( {
@@ -16,4 +19,11 @@ gulp.task( 'clean', function () {
      .pipe( gulp.dest( 'target' ) )
  } );
 
- gulp.task( 'default', [ 'clean', 'nunjucks' ] );
+//Compile sass, put in target/css
+ gulp.task( 'sass', function() {
+     return gulp.src( './sass/**/*.scss' )
+     .pipe( sass().on( 'error', sass.logError ) )
+     .pipe( gulp.dest( './target/css' ) );
+ } );
+
+ gulp.task( 'default', [ 'clean', 'nunjucks', 'sass' ] );
