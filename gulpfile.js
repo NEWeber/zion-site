70ce6726a2a355c6c2f6a465ee127872e43230aa
Nicholas Weber <nicholaseweber@gmail.com>
2016-10-30 15:26:53 -0700

    Add gulp task to minify js and update gulp task.

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,10 @@
 var gulp           = require( 'gulp' );
+var pump           = require('pump');
 var del            = require( 'del' );
 var nunjucksRender = require( 'gulp-nunjucks-render' );
 var sass           = require( 'gulp-sass' );
+var uglify         = require('gulp-uglify');
+//TODO: change all tasks to use pump
 
 //Delete old files in target
 gulp.task( 'clean', function ( cb ) {
@@ -27,10 +30,20 @@ gulp.task( 'clean', function ( cb ) {
      .pipe( gulp.dest( './target/css' ) );
  } );
 
+ gulp.task( 'compress', [ 'clean' ], function ( cb ) {
+  pump( [
+        gulp.src( './js/*.js' ),
+        uglify(),
+        gulp.dest( './target/js' )
+    ],
+    cb
+  );
+} );
+
 // checkout https://github.com/sindresorhus/gulp-imagemin for images
  gulp.task( 'copy', [ 'clean' ], function() {
      gulp.src( './img/*' )
      .pipe( gulp.dest( './target/img' ) );
  } );
 
- gulp.task( 'default', [ 'clean', 'nunjucks', 'sass', 'copy' ] );
+ gulp.task( 'default', [ 'clean', 'nunjucks', 'sass', 'compress', 'copy' ] );
