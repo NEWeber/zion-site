73bc9fce4a0f87b1bac69403687fef92b126a4ec
Nicholas Weber <nicholaseweber@gmail.com>
2016-11-20 20:55:23 -0800

    responsive top nav WIP, main nav working.

--- js/main.js
+++ js/main.js
@@ -1,10 +1,19 @@
-$( window ).scroll( function () {
-    var scrollMeasure = $(window).scrollTop();
+// $( window ).scroll( function () {
+//     var scrollMeasure = $(window).scrollTop();
+//
+//     if(scrollMeasure >= 20) {
+//         $( '.menu-wrap' ).addClass( 'menu-wrap-scrolled' );
+//
+//     } else {
+//         $( '.menu-wrap' ).removeClass( 'menu-wrap-scrolled' );
+//     }
+// } );
 
-    if(scrollMeasure >= 20) {
-        $( '.menu-wrap' ).addClass( 'menu-wrap-scrolled' );
-
+function myFunction() {
+    var topnav = document.getElementById("my-top-nav");
+    if (topnav.className === "topnav") {
+        topnav.className += " responsive";
     } else {
-        $( '.menu-wrap' ).removeClass( 'menu-wrap-scrolled' );
+        topnav.className = "topnav";
     }
-} );
+}
