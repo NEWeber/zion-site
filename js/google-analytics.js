d0254cd4b8f6cbf6fd1cf306d6ccefd49f7f748a
Nicholas Weber <nicholaseweber@gmail.com>
2026-07-21 13:53:08 -0500

    Update Google Analytics ID

--- js/google-analytics.js
+++ js/google-analytics.js
@@ -1,7 +1,20 @@
-(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+(function () {
+  var measurementId = 'G-9WLJSTXBK6';
 
-ga('create', 'UA-86565930-1', 'auto');
-ga('send', 'pageview');
+  window.dataLayer = window.dataLayer || [];
+
+  function gtag() {
+    dataLayer.push(arguments);
+  }
+
+  window.gtag = gtag;
+
+  gtag('js', new Date());
+  gtag('config', measurementId);
+
+  var script = document.createElement('script');
+  script.async = true;
+  script.src = 'https://www.googletagmanager.com/gtag/js?id=' + measurementId;
+
+  document.head.appendChild(script);
+})();
